eeec1baca1c9c19315af8a77beaee3e609e42645
deepu991 <137867189+deepu991@users.noreply.github.com>
2023-06-27 22:01:51 +0530

    Delete execel1 directory

--- execel1/filemanager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'filemanager'
-
-urlpatterns = [
-    path('upload/', views.upload_file, name='upload'),
-    path('download/', views.download_file, name='download'),
-]
